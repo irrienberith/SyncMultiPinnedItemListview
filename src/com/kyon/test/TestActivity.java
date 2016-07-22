b4b71e1d2867b75ec56ccd90989028b0238d2b2e
kyon <neon9109@gmail.com>
2016-07-22 14:47:58 +0800

    initial Commit

---
+++ src/com/kyon/test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.kyon.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kyon on 16-7-22.
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
